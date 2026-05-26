62d8ba9c4e06ac34005a4f480c56e1aeb7f46a11
Sepehr Hajimokhtar <sepehrmokhtar@icloud.com>
2026-05-26 11:11:16 +0200

    server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://127.0.0.1:3001",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
